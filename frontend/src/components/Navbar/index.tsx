168396fbd77e7e4c77b1699946d6514850d371fb
mpk0762 <mpk86647@gmail.com>
2022-12-01 19:58:14 -0300

    iniciando navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,15 @@
+import "bootstrap/js/src/collapse.js";
+import "./styles.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar main-nav">
+     <h1>navbar</h1>
+    
+    </nav>
+  
+  );
+};
+export default Navbar;
+
+
